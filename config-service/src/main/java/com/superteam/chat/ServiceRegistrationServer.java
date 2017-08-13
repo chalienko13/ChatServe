2466c85f6b5a9f741247ec28ac138450f1e4b4e1
dmitr <dmitriy.chalienko@gmail.com>
2017-08-13 12:14:50 +0300

    Initial commit

---
+++ config-service/src/main/java/com/superteam/chat/ServiceRegistrationServer.java
@@ -0,0 +1,18 @@
+package com.superteam.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableZuulProxy
+public class ServiceRegistrationServer {
+
+    public static void main(String[] args) {
+        // Tell Boot to look for registration-server.yml
+        System.setProperty("spring.config.name", "application");
+        SpringApplication.run(ServiceRegistrationServer.class, args);
+    }
+}
